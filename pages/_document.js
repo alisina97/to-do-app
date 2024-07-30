578a9ccb47d28f32d9c5f5740bde942df3df46a2
Ali Sina <ali.sina1447@gmail.com>
2024-07-30 01:18:41 -0400

    [UPDATE] removed default next js code and initialized a custom app component

--- pages/_document.js
+++ pages/_document.js
@@ -3,9 +3,16 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+      <link 
+          rel="stylesheet" 
+          href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" 
+        />
+      </Head>
       <body>
-        <Main />
+        <div className="container">
+          <Main />
+        </div>
         <NextScript />
       </body>
     </Html>
